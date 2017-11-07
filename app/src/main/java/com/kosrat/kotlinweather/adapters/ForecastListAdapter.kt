867c6fae92a142f00ffdc97884461e7380fa3a38
Kosrat D. Ahmad <kosrat.d.ahmad@gmail.com>
2017-11-07 10:46:08 +0300

    feat: Add recycler view for forecast list

---
+++ app/src/main/java/com/kosrat/kotlinweather/adapters/ForecastListAdapter.kt
@@ -0,0 +1,26 @@
+package com.kosrat.kotlinweather.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ *
+ * Created by kosrat on 11/7/17.
+ */
+
+class ForecastListAdapter(private var item: List<String>) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            = ViewHolder(TextView(parent.context))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.text = item[position]
+    }
+
+    override fun getItemCount() = item.size
+
+    class ViewHolder(var text: TextView) : RecyclerView.ViewHolder(text)
+
+}
